fe98f9e540d48f8cdff329b52ad08819918da763
Artem <applyyap@gmail.com>
2026-07-14 16:55:18 +0900

    Fix: message

--- server/sockets.js
+++ server/sockets.js
@@ -12,13 +12,20 @@ module.exports = (io) => {
 
     socket.on('sendMessage', async (data) => {
       try {
-        const { chatId, text, senderId, partnerId } = data;
+        const { chatId, text, senderId, partnerId, tempId } = data;
 
         const newMessage = new Message({ chatId, senderId, text });
         const savedMessage = await newMessage.save();
         
         const populatedMessage = await Message.findById(savedMessage._id)
           .populate('senderId', 'name avatar username');
+
+        // Подтверждение для отправителя с реальным _id
+        socket.emit('messageSent', { 
+          _id: savedMessage._id, 
+          chatId, 
+          tempId: tempId || null 
+        });
 
         // Отправляем сообщение всем в комнате чата
         io.to(chatId).emit('newMessage', populatedMessage);
